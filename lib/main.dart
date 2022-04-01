e76e8a3acd00fd4103a1f2086ee46f4641669b75
Akash701 <akashjnair701@gmail.com>
2022-04-01 16:39:46 +0530

    Api data fetched

--- lib/main.dart
+++ lib/main.dart
@@ -6,5 +6,5 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(MaterialApp(home: ApiCall()));
+  runApp(MaterialApp(home: ApiCall())); //use MyApp() to start from first page
 }
